81b066050d1c92fdb96883f802192ff6e69a4f12
Alexander Zhukovsky <zhukovsky.alexander@gmail.com>
2020-10-29 22:37:24 +0300

    ENH: template v2

--- otsu.py
+++ otsu.py
@@ -1,13 +1,21 @@
-from __future__ import print_function
-from sys import argv
-import os.path
+import sys, os.path, cv2, numpy as np
 
 
-def otsu(src_path, dst_path):
-    pass
+def otsu(img: np.ndarray) -> np.ndarray:
+    pass  # insert your code here
+
+
+def main():
+    assert len(sys.argv) == 3
+    src_path, dst_path = sys.argv[1], sys.argv[2]
+
+    assert os.path.exists(src_path)
+    img = cv2.imread(src_path, cv2.IMREAD_GRAYSCALE)
+    assert img is not None
+
+    result = otsu(img)
+    cv2.imwrite(dst_path, result)
 
 
 if __name__ == '__main__':
-    assert len(argv) == 3
-    assert os.path.exists(argv[1])
-    otsu(*argv[1:])
+    main()
